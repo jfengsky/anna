a4686956615cea67e10faa2259e2aac8f01f38ce
姜峰 <jiang.f@Ctrip.com>
2017-03-23 10:42:30 +0800

    test prettier

---
+++ public/test/test.js
@@ -0,0 +1,12 @@
+const abc = "aaaa"
+
+const add =  (a,b) => {
+    return a + b
+}
+
+const reduce=(a,b)=>a-b
+
+const tpl=`<div>
+<p>111</p>
+<span></span>
+</div>`
---
+++ public/test/test2.js
@@ -0,0 +1,11 @@
+var tpl = "<div>\
+<p>111</p>\
+<div>"
+
+function add(a,b){
+    return a+b
+}
+
+function reduce(a,b){
+return a-b
+}
