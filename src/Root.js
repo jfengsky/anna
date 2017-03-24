a37f9e6830a06a469a77dbac1495bffa9370105c
姜峰 <jiang.f@Ctrip.com>
2017-03-24 15:08:19 +0800

    add root

---
+++ src/Root.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react'
+
+class Root extends Component {
+  render() {
+    return (
+      <div>
+        Root
+      </div>
+    )
+  }
+}
+
+Root.propTypes = {}
+
+export default Root
