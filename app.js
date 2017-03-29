e056af9f3005a7cea7ca3465b651987a315dbbb3
姜峰 <jiang.f@Ctrip.com>
2017-03-29 18:46:16 +0800

    modify

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-import express  from 'express'
-import path from 'path'
-import favicon from 'serve-favicon';
-import logger from 'morgan'
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
-import ReactView from 'express-react-views'
-import serveStatic from 'serve-static'
-import index from './routes/index'
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const app = express()
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'jade');
-app.set('view engine', 'js')
-app.engine('js', ReactView.createEngine({
-  beautify: true
-}))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(serveStatic(path.join(__dirname, 'dist')))
-app.use('/', index)
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
-  err.status = 404
-  next(err)
-})
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -43,7 +41,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-})
+});
 
-export default app
-
+module.exports = app;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// export default (params) => (
-//     <div>root</div>
-// )
-
-const root = param => <div>renderToString</div>
-
-export default content
